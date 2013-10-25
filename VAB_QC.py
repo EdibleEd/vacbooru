72a49cf63ec7fcf1f3e72926f55377f1bbd78cdd
Till Elton <edible756@gmail.com>
2013-10-26 05:30:11 +1300

    Added stubs for QC
    Added comments and method stubs for VAB_QC
    Also start of commandline arg handling

--- VAB_QC.py
+++ VAB_QC.py
@@ -14,4 +14,44 @@
 # 5. verify checksum is sane
 
 class VAB_QC:
-    pass
+
+	def __init__(self, args):
+		# if in interactive mode, prompt the user to perform actions
+		if(args.interactive):
+			mainLoop(args)
+			
+		# Otherwise perform all automated tasks specified
+		else:
+			pass
+			
+	# Interactive mode, 
+	def mainLoop(self, args):
+		pass
+	
+	# Cull a set of tags from the data
+	def cullTags(self, data, tags):
+		pass
+	
+	# Add a set of tags to the data
+	def addTags(self, data, tags):
+		pass
+		
+	# Add a user tag to the data
+	def addUser(self, data, user):
+		pass
+	
+	# Interactively add tags to data
+	def editTags(self, data):
+		pass
+		
+	# Verify that the data has a correctly calculated checksum
+	def verifyChecksum(self, data):
+		pass
+		
+		
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Cull an image list')
+	parser.add_argument('tags', metavar='N', nargs='+', help='A single tag list for the program')
+	parser.add_argument('-i', '-interactive',type=bool help="Set if running in interactive mode")
+	args = parser.parse_args()
+	QC(args)
