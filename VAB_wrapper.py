b96b19a06728167ac2f19882ccfe772cb60988bc
Till Elton <edible756@gmail.com>
2013-10-28 00:59:10 +1300

    Added VAB_wrapper, a wrapper to call the toolchain
    A wrapper class that will call all the others in the toolchain, and handle all configuration
    file based issues, as well as passing information between and possibly some user interaction

---
+++ VAB_wrapper.py
@@ -0,0 +1,31 @@
+
+
+# Wrapper class that runs the whole shabang
+# Will take data from a bunch of configs primarily
+
+class VAB_wrapper:
+    
+    def __init__(self):
+        pass
+
+    def loadFolder(self):
+        pass
+
+    def IQDBCall(self):
+        pass
+
+    def QC(self):
+        pass
+
+    def upload(self):
+        pass
+
+    def chain(self):
+        pass
+
+    def prepareConfigs(self):
+        pass
+
+if __name__ == '__main__':
+    wrap = VAB_wrapper()
+    
