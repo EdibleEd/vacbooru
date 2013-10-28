d9ef1248f633a44a100b2f15880ee125c3ab274d
Till Elton <edible756@gmail.com>
2013-10-29 03:06:33 +1300

    Added config file loader
    A simple tab seperated config file loader with # comments

--- Utility.py
+++ Utility.py
@@ -56,10 +56,28 @@ def loadNetworkConfig(netFile):
 		useProxy = False
 	return (useProxy, proxyAddr, int(proxyPort), username, password)
 
-
-
 # Prints if the level of debug printing is greater or equal to this methods threshold to be printed
 # typically 0 = never, 1 = standard running, 2-5 = various debug levels
 def debugPrint(message, level, threshold):
 	if (level >= threshold):
 		print(message)
+        
+
+# Loads configuration dictionaries
+# Uses # for comments
+# Uses tab separators
+
+def loadSimpleConfig(configF):
+    configDict = {} 
+    for line in configF:
+        if (not(line.startswith("#"))): # Do not add comments
+            if (not(line.strip() == '')): # Don't add whitespace lines or empty lines
+                line = line.rstrip('\n') # Don't want newlines
+                templ = line.split(None,1)
+                if (len(templ[1].split(',')) != 1): # Load comma seperated values as lists
+                    configDict[templ[0].strip()] = templ[1].split(',') # Put the first value as the key, then rest as values
+                else:
+                    configDict[templ[0].strip()] = (templ[1].strip()) # strips are to get rid of lose lines and whitespace
+    return configDict
+
+        
