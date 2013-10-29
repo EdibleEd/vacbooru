981db6a01f0dfff008a4963c7aa00bd9a5386945
Till Elton <edible756@gmail.com>
2013-10-29 19:00:02 +1300

    Added arg to wrapper for config
    Defaults to cwd/Args/main.conf

--- VAB_wrapper.py
+++ VAB_wrapper.py
@@ -4,18 +4,21 @@ import VAB_upload
 import utility
 import VAB_loadfolder
 import os
-
+import argparse
 
 # Wrapper class that runs the whole shabang
 # Will take data from a bunch of configs primarily
 
 class VAB_wrapper:
 
-    def __init__(self):
+    def __init__(self, args):
+        if (args.config == None):
+            with open(os.path.join(os.getcwd(), "Config", "main.conf"), 'r', encoding='utf-8') as mainConfig:
+                mainConf = loadSimpleConfig(config)
+        else:
+            with open(args.config), 'r', encoding='utf-8') as mainConfig:
+                mainConf = loadSimpleConfig(config)
 
-        with open(os.path.join(os.getcwd(), "Config", "main.conf"), 'r', encoding='utf-8') as mainConfig:
-            mainConf = loadSimpleConfig(config)
-            
     def loadFolder(self, config):
         pass
 
@@ -55,6 +58,8 @@ class VAB_wrapper:
         upload(upload_config)        
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Import and upload a folder of images to vacbooru')
+	parser.add_argument("config", help="Path to configuration script")
     wrap = VAB_wrapper()
     wrap.chain()
     
