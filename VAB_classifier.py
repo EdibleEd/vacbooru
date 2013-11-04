bdb1cb11c36f9c43d83bcb146029b8f005f82272
Kieran Carnegie <kotarou@ecs.vuw.ac.nz>
2013-11-04 14:44:48 +1300

    Classifier modifications - Added curvature tests - Added comments

--- VAB_classifier.py
+++ VAB_classifier.py
@@ -4,6 +4,8 @@ import numpy as np_
 import atexit
 import time 
 from math import exp
+import scipy.signal as ss_
+import scipy.misc as sm_
 
 class VAB_classifier:
 
@@ -12,6 +14,9 @@ class VAB_classifier:
 	def sigmoid(y):
 		return (1 / (1 + exp(-y)))
 	
+	# Comparator that creates a 6vector feature set
+	# 	<meanR meanB meanG varR varB varG>
+	# Performance on basic set is (117.4113 0.0014)
 	def rbgMV(image):
 		rows = image.size[0]
 		cols = image.size[1]
@@ -36,24 +41,53 @@ class VAB_classifier:
 		var[2] = var[2] / npixels
 		out = [mean[0], mean[1], mean[2], var[0], var[1], var[2]]
 		return out 
-	
-	def valForBS(features):
-		# a = sigmoid(features[0])
-		# b = sigmoid(features[1])
-		# c = sigmoid(features[2])
-		# d = sigmoid(features[3])
-		# e = sigmoid(features[4])
-		# f = sigmoid(features[5])
-		a = (1. / (1 + exp(-features[0])))
-		b = (1. / (1 + exp(-features[1])))
-		c = (1. / (1 + exp(-features[2])))
-		d = (1. / (1 + exp(-features[3])))
-		e = (1. / (1 + exp(-features[4])))
-		f = (1. / (1 + exp(-features[5])))
-		return a + b + c + d + e + f
-		# signoid (each feature)
-		# then do binary search onit
 
+	# Compares image curvature in each of the R G and B pixel colours
+	# Does not cope well with big image modifications like erasing sections
+	# Performance on basic set is (117.6904459 0.00297808647156)
+	# Or for actual scaled images (117.495517015 0.00292205810547)
+	def curvature(image):
+		r, g, b = image.split()
+		rArray = np_.array(r)
+		gArray = np_.array(g)
+		bArray = np_.array(b)
+		l = [[0, -1, 0], [-1, 4, -1], [0, -1, 0]]
+		rCurve = ss_.convolve2d(rArray, l)
+		gCurve = ss_.convolve2d(gArray, l)
+		bCurve = ss_.convolve2d(bArray, l)
+		cr = 0
+		cg = 0
+		cb = 0
+		for i in range(len(rCurve)):
+			for j in range(len(rCurve[0])):
+				cr += rCurve[i, j]**2
+				cg += gCurve[i, j]**2
+				cb += bCurve[i, j]**2
+		return [cr, cb, cg]
+
+
+	# This is the baseline comparator
+	# Compare each pixel value to the corresponding pixel
+	# 	Or if there is no pixel there due to aspect ratio difference, return maximum difference
+	# Performance on basic set is (96.8937, 26.0121) (higher is worse)
+	def pixDiff(image, image2):
+		rows = image.size[0]
+		cols = image.size[1]
+		out = 0
+		for i in range(rows):
+			for j in range(cols):
+				try:
+					r, g, b = image.getpixel((i, j))
+					r1, g1, b1 = image2.getpixel((i, j))
+					out += ((r-r1)**2 + (g-g1)**2 + (b-b1)**2)
+				except Exception:
+					# the images are of different ratios
+					# maximised error on any pixel that is not common to both
+					out += ((255**2) * 3)
+		return out 
+
+	# This is needed because the source and target images we want to find exists within the test folder
+	# And the source needs to be discarded 
 	def sec_lowest(dicti):
 		minVal = 9e19
 		minKey = ''
@@ -76,7 +110,8 @@ class VAB_classifier:
 		        im = Image.open(infile).convert('RGB')
 		        im.thumbnail(size, Image.ANTIALIAS)
 		        im.save(outfile, "png")
-		        fileDict[infile] =  valForBS(rbgMV(Image.open(outfile)))
+		        fileDict[infile] = curvature(im)
+		        #fileDict[infile] =  (rbgMV(im))
 		    except Exception as e: #IOError
 		       print "cannot create thumbnail for '%s'" % infile
 		       print e
@@ -85,10 +120,12 @@ class VAB_classifier:
 		# 	continue
 	time1 = time.time()
 	for key in fileDict.keys():
+		#dist = (fileDict[key] - fileDict['123_mod.jpg'])**2
+		
 		dist = 0
-		for i in range(6):
-			#dist += (fileDict[key][i] - fileDict['123_mod.jpg'][i])**2
-			dist += (fileDict[key] - fileDict['123_mod.jpg'])**2
+		for i in range(3):
+			dist += (fileDict[key][i] - fileDict['123_mod4.jpg'][i])**2
+			#dist += (fileDict[key] - fileDict['123_mod.jpg'])**2
 		distDict[key]= dist
 	print sec_lowest(distDict)
 	time2 = time.time()
